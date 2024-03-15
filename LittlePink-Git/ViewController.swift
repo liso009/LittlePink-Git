09e205cf57fff99b8207c8680a043742f45024ce
Shichao Huang <282301947@qq.com>
2024-03-15 23:33:39 +0800

    做了UI

--- LittlePink-Git/ViewController.swift
+++ LittlePink-Git/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("做了UI")
         // Do any additional setup after loading the view.
     }
 
